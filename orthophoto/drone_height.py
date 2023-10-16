8f2516b4027ba944f11953127ed6072e6e568d6f
dkccccc <dkc@kakao.com>
2023-10-17 01:27:59 +0900

    드론 높이 값 구하는 코드 추가 작성(진행 중)

--- orthophoto/drone_height.py
+++ orthophoto/drone_height.py
@@ -1,26 +1,6 @@
-# 특정시간에 해수면 위에 떠 있는 드론의 정확한 높이 값을 추출하기 위한 코드
-
-# 전제 조건
- # 드론 스틸사진의 절대고도(Absolute Altitude)를 이용한다.
- # 절대고도(Absolute Altitude)는 일반적인 평균해수면 기준의 정표고이지만,
- # 드론의 기종에 따라서 실제는 타원체를 기준으로 하는 경우도 있다.
-
-
-# 01. 드론 스틸사진에서 추출한 절대고도(Absolute Altitude), 촬영 시간 등 필요한 메타 데이터를 추출한다.
-# 02. 절대고도(Absolute Altitude)가 평균해수면을 기준일 경우
- # 02-1. 사진이 촬영된 시간의 조위값을 가져온다(해양수산부 바다누리 해양정보서비스).
-  # http://www.khoa.go.kr/oceangrid/khoa/takepart/openapi/openApiObsTideRealDataInfo.do
- # 02-2. 사진이 촬영된 위치(좌표)의 평균해수면과 기본수준면(조위기준)의 차이 값을 가져온다.(국토지리정보원 육상/해상 높이 연계 서비스)
-  # https://map.ngii.go.kr/ms/mesrInfo/geoidIntro.do "그리드 입력 기능으로 텍스트 파일 다운로드"=>CSV 파일로 변경
- # 02-3. 조위값과 기준면의 차이를 이용해서 드론과 해수면과의 높이를 구한다. 
- # 드론의 높이(Height) =  절대고도(Absolute Altitude) - (조위 - (평균해수면 - 기본수준면))
-# 03. 절대고도(Absolute Altitude)가 타원체고 일 경우(DJI 기종)
- # 03-1. "KNGeoid18.dat"에서 촬영 위치(좌표)의 지오이드고를 가져온다(국토지리정보원 수직기준전환 서비스에서 다운로드).
-  # https://map.ngii.go.kr/ms/mesrInfo/geoidIntro.do 
-##이후의 과정은 02.의 순서를 그대로 따른다.
-# 04. 최종으로 계산한 드론과 해수면의 높이 차이를 반환한다.
 
 from scipy.spatial import cKDTree
+import requests
 
 # CSV 파일 읽기
  # 위도, 경도, LDL-IMSL, LDL-LMSL, LMSL-IMSL, CSV 파일 읽어오기 "LDL(수준(조위)기준면), IMSL(인천만평균해수면), LMSL(지역평균해수면)"
@@ -40,12 +20,49 @@ def create_kd_tree(data):
     kdtree = cKDTree(coordinates)
     return kdtree
 
-# API를 이용해 조위값 가져오는 함수
-def get_tide_level(time):
+# API를 이용해 조위값 가져오는 함수 #time은 분단위로 내림
+def get_tide_level(ObsCode, date, time):
+
+    # API 요청을 보낼 주소
+    api_url = "http://www.khoa.go.kr/api/oceangrid/tideObs/search.do"
+
+    # API 키
+    api_key = "YmHupI3Nga4UvQn7gONpA=="
+
+    # 요청 파라미터 설정
+    obs_code = ObsCode
+    date = date
+    result_type = "json"
+    formatted_date = f"{date[:4]}-{date[4:6]}-{date[6:]}" #요청할 때 날짜형식과 결괏값의 날짜 형식이 달라서 결과 찾을 날짜 형식으로 변환
+
+    # API 요청 보내기
+    response = requests.get(api_url, params={"ServiceKey": api_key, "ObsCode": obs_code, "Date": date, "ResultType": result_type})
+
+    # API 응답 확인
+    if response.status_code == 200:
+        # JSON 형태로 응답을 받아옴
+        data = response.json()
+        # 원하는 데이터 추출
+        # print(data)
+
+        target_time = formatted_date + " " + time  # 가져오고자 하는 특정 시간
+        tide_levels = data["result"]["data"]
+
+        for record in tide_levels:
+            if record["record_time"] == target_time:
+                tide_level = record["tide_level"]
+                print(f"특정 시간 {target_time}의 조위 값: {tide_level}")
+                break
+        else:
+            print(f"특정 시간 {target_time}의 조위 값이 없습니다.")
+
+    else:
+        print("API 요청 실패")
+    
     return tide_level
     
 
-def drone_height(file_path, latitude, longitude, absolutaltitude, time):
+def drone_height(file_path, latitude, longitude, absolutaltitude, date, time, ObsCode):
   
   # CSV 파일에서 데이터 읽어오기
   data = read_csv(file_path)
@@ -55,10 +72,23 @@ def drone_height(file_path, latitude, longitude, absolutaltitude, time):
   distance, index = kdtree.query([latitude, longitude])
   closest_point = data[index]
 
-  tide_level = get_tide_level(time)
+  tide_level = get_tide_level(ObsCode, date, time)
 
   # 드론의 높이(Height) =  절대고도(Absolute Altitude) - (조위 - (평균해수면 - 기본수준면))
-  height = absolutaltitude - (tide_level - closest_point[2])
+  height = float(absolutaltitude) - (float(tide_level) - float(closest_point[2]))
 
   return height
+
+if __name__ == "__main__":
+
+    file_path = "./IMSL_LDL_ETC_jeju.csv"
+    ObsCode = "DT_0001"
+    date = "20231017"
+    time = "00:15:00"
+    latitude = "37.45194"
+    longitude = "126.59222"
+    absolutaltitude = "100"
+    height = drone_height(file_path, latitude, longitude, absolutaltitude, date, time, ObsCode)
+    print("drone height : ", height)
+    
 
---
+++ orthophoto/test.py
@@ -0,0 +1,35 @@
+import requests
+
+# API 요청을 보낼 주소
+api_url = "http://www.khoa.go.kr/api/oceangrid/tideObs/search.do"
+
+# API 키
+api_key = "YmHupI3Nga4UvQn7gONpA=="
+
+# 요청 파라미터 설정
+obs_code = "DT_0001"
+date = "20231017"
+result_type = "json"
+
+# API 요청 보내기
+response = requests.get(api_url, params={"ServiceKey": api_key, "ObsCode": obs_code, "Date": date, "ResultType": result_type})
+
+# API 응답 확인
+if response.status_code == 200:
+    # JSON 형태로 응답을 받아옴
+    data = response.json()
+    # 원하는 데이터 추출
+    target_time = "2023-10-17 00:37:00"  # 가져오고자 하는 특정 시간
+    tide_levels = data["result"]["data"]
+
+    for record in tide_levels:
+        if record["record_time"] == target_time:
+            tide_level = record["tide_level"]
+            print(f"특정 시간 {target_time}의 조위 값: {tide_level}")
+            break
+    else:
+        print(f"특정 시간 {target_time}의 조위 값이 없습니다.")
+
+else:
+    print("API 요청 실패")
+
