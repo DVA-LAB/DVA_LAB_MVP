af1f06a70f3ad34cd5a7f8af67e4b766786ef89e
Seowoo Han <swhan0329@gmail.com>
2023-10-23 13:39:02 +0900

    Save right version

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,43 +1,86 @@
-import React, { Component } from 'react';
+import React, { Component, useRef, useEffect } from 'react';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      videoSrc: null,             // 비디오 파일 Blob 객체
-      videoPlaying: false,        // 비디오 재생 상태
-      frameNumber: 0,             // 현재 프레임 번호
-      addingInfo: false,          // 추가 정보 입력 모드 활성화 여부
-      points: [],                 // 입력한 점 좌표
-      pointColors: [],            // 입력한 점 색상
-      pointDistances: [],         // 입력한 점 사이의 거리
-      distanceInputs: [],         // 입력한 거리 값
+      videoSrc: null,
+      logFile: null,
+      videoPlaying: false,
+      frameNumber: 0,
+      addingInfo: false,
+      points: [],
+      pointColors: [],
+      pointDistances: [],
+      distanceInputs: [],
+      allFillUpload: false,
+      aiModelActive: false,
     };
 
     this.videoRef = React.createRef();
     this.fileInputRef = React.createRef();
+    this.logFileInputRef = React.createRef();
+    this.canvasRef = React.createRef();
+    this.clearCanvas = this.clearCanvas.bind(this);
+    this.drawPointsAndLines = this.drawPointsAndLines.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const video = this.videoRef.current;
+    const canvas = this.canvasRef.current;
+
+    if (canvas && video) {
+      if (!this.context) {
+        this.context = canvas.getContext('2d');
+        video.addEventListener('loadeddata', this.handleVideoLoaded);
+      }
+      canvas.width = video.offsetWidth;
+      canvas.height = video.offsetHeight;
+    } else {
+      console.error("Canvas or Video element is not available.");
+    }
+  }
+
+  handleVideoLoaded = () => {
+    const video = this.videoRef.current;
+    const canvas = this.canvasRef.current;
+    if (canvas && video) {
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      this.drawInterval = setInterval(this.drawPointsAndLines, 100);
+    }
+  };
+
+  // clearCanvas 함수 수정
+  clearCanvas() {
+    const canvas = this.canvasRef.current;
+
+    if (canvas) {
+      const context = canvas.getContext('2d');
+      if (context) {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+      } else {
+        console.error("Canvas context is not available.");
+      }
+    } else {
+      console.error("Canvas element is not available.");
+    }
   }
 
   // 비디오 파일 로드
   loadVideo = () => {
     const { videoSrc } = this.state;
-
     if (videoSrc) {
       this.videoRef.current.src = URL.createObjectURL(videoSrc);
       this.videoRef.current.load();
       this.videoRef.current.addEventListener('loadeddata', () => {
-        // 비디오 데이터 로드 후 1번째 프레임으로 이동
         this.videoRef.current.currentTime = 0;
         this.setState({ videoPlaying: false });
-
-        // 비디오 크기 조절
-        this.videoRef.current.width = window.innerWidth; // 화면 너비에 맞게 조절
-        this.videoRef.current.height = window.innerHeight; // 화면 높이에 맞게 조절
       });
-      this.videoRef.current.addEventListener('timeupdate', this.updateFrameNumber);
     }
   };
+
 
   // 파일 업로드 핸들러
   handleFileUpload = (e) => {
@@ -46,6 +89,15 @@ class App extends Component {
       this.setState({ videoSrc: file }, () => {
         this.loadVideo();
       });
+    }
+  };
+
+  // 로그 파일 업로드 핸들러
+  handleLogFileUpload = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      this.setState({ logFile: file });
+      this.setState({ allFillUpload: true });
     }
   };
 
@@ -62,7 +114,10 @@ class App extends Component {
 
   // 현재 프레임 번호 업데이트
   updateFrameNumber = () => {
-    this.setState({ frameNumber: Math.floor(this.videoRef.current.currentTime * 30) }); // 30fps 기준
+    const { videoSrc } = this.state;
+    if (videoSrc) {
+      this.setState({ frameNumber: Math.floor(this.videoRef.current.currentTime * 30) }); // 30fps 기준
+    }
   };
 
   // 추가 정보 입력 모드 활성화/비활성화 토글
@@ -70,38 +125,118 @@ class App extends Component {
     this.setState((prevState) => ({ addingInfo: !prevState.addingInfo }));
   };
 
-  // 비디오 클릭 시 점 추가
+  // handleVideoClick 함수 수정
   handleVideoClick = (e) => {
     if (this.state.addingInfo) {
-      const { clientX, clientY } = e;
-      const rect = this.videoRef.current.getBoundingClientRect();
+      const rect = this.canvasRef.current.getBoundingClientRect();
+      const { clientX, clientY } = e.nativeEvent;
       const x = clientX - rect.left;
       const y = clientY - rect.top;
-      const points = [...this.state.points, { x, y }];
-      const pointColors = [...this.state.pointColors, `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`];
-      this.setState({ points, pointColors }, () => {
-        if (points.length % 2 === 0) {
-          // 두 점을 클릭한 경우
-          this.calculateDistance(points.length - 2);
+
+      this.setState((prevState) => {
+        const points = [...prevState.points, { x, y }];
+        const pointColors = [...prevState.pointColors, `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`];
+
+        return { points, pointColors };
+      }, () => {
+        // Check if there are even points and more than 1 point
+        if (this.state.points.length % 2 === 0 && this.state.points.length > 1) {
+          const lastPointIndex = this.state.points.length - 1;
+          // Calculate the distance for the last two points
+          this.calculateDistance(lastPointIndex); // Calculate distance for the previous pair of points
         }
+
+        // Draw points and lines after adding the second point
+        this.drawPointsAndLines();
       });
     }
   };
 
-  // 두 점 사이의 거리 계산
+  // calculateDistance 함수 수정
   calculateDistance = (pointIndex) => {
     const { points } = this.state;
-    const point1 = points[pointIndex];
-    const point2 = points[pointIndex + 1];
+    console.log(points);
+    const point1 = points[pointIndex - 1];
+    const point2 = points[pointIndex];
+
     const distancePixels = Math.sqrt((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2);
-    const distanceMeters = parseFloat(prompt(`Enter distance between point ${pointIndex / 2 + 1} and point ${pointIndex / 2 + 2} in meters:`));
-    if (!isNaN(distanceMeters)) {
-      const pointDistances = [...this.state.pointDistances, { meters: distanceMeters, pixels: distancePixels }];
-      this.setState({ pointDistances });
-    } else {
-      alert('Invalid input. Please enter a valid number for the distance.');
-    }
+
+    // Prompt for distance input after drawing points and lines
+    setTimeout(() => {
+      const distanceMeters = parseFloat(prompt(`Enter distance between point ${pointIndex - 1} and point ${pointIndex} in meters:`));
+      if (!isNaN(distanceMeters)) {
+        const pointDistances = [...this.state.pointDistances, { meters: distanceMeters, pixels: distancePixels }];
+        this.setState({ pointDistances }, () => {
+          // 상태 업데이트 후에 점과 선을 다시 그립니다.
+          this.drawPointsAndLines();
+        });
+      } else {
+        alert('Invalid input. Please enter a valid number for the distance.');
+      }
+    }, 0);
   };
+
+  // << 10 seconds backward 버튼 클릭 핸들러
+  skipBackward = () => {
+    this.videoRef.current.currentTime -= 10;
+  };
+
+  // >> 10 seconds forward 버튼 클릭 핸들러
+  skipForward = () => {
+    this.videoRef.current.currentTime += 10;
+  };
+
+  // AI 모델 실행 핸들러
+  runAIModel = () => {
+    // AI 모델 실행 로직 추가
+    setTimeout(() => {
+      // AI 모델 실행 후, << 10 seconds backward, play(pause), >> 10 seconds forward 버튼 활성화
+      this.setState({ aiModelActive: true });
+    }, 1000);
+  };
+
+  // 점과 선 그리기 함수
+  drawPointsAndLines() {
+    const canvas = this.canvasRef.current;
+    if (!canvas) {
+      console.error("Canvas element is not available.");
+      return;
+    }
+
+    const context = canvas.getContext('2d');
+    if (!context) {
+      console.error("Canvas context is not available.");
+      return;
+    }
+
+    const { points, pointColors, addingInfo } = this.state;
+
+    for (let i = 0; i < points.length; i++) {
+      // 현재 점의 색상을 설정합니다.
+      context.fillStyle = pointColors[i % 2 === 0 ? i : i - 1];
+      context.beginPath();
+      context.arc(points[i].x, points[i].y, 10, 0, Math.PI * 2);
+      context.fill();
+
+      // 짝수 인덱스일 때 선을 그립니다.
+      if (i % 2 === 1 && i > 0) {
+        // 이전 점의 색상을 사용하여 선의 색상을 설정합니다.
+        context.strokeStyle = pointColors[i - 1];
+        context.lineWidth = 2;
+        context.beginPath();
+        context.moveTo(points[i - 1].x, points[i - 1].y);
+        context.lineTo(points[i].x, points[i].y);
+        context.stroke();
+
+        // 선에 텍스트를 추가합니다.
+        // 이전 점의 색상을 사용하여 텍스트의 색상을 설정합니다.
+        context.fillStyle = pointColors[i - 1];
+        context.font = "20px Arial";
+        // 선의 중간에 라벨을 추가합니다.
+        context.fillText(`line${(i + 1) / 2}`, (points[i - 1].x + points[i].x) / 2, (points[i - 1].y + points[i].y) / 2 - 10);
+      }
+    }
+  }
 
   // 렌더링
   render() {
@@ -112,31 +247,63 @@ class App extends Component {
     const videoStyle = {
       width: '100%', // 부모 요소에 맞게 너비 조절
       height: '80%', // 부모 요소에 맞게 높이 조절
+      position: 'relative',  // 이 줄을 추가하세요
+      zIndex: 0  // 이 줄을 추가하세요
     };
-    const { videoPlaying, frameNumber, addingInfo, pointColors, pointDistances } = this.state;
+    const { videoPlaying, frameNumber, addingInfo, pointColors, pointDistances, allFillUpload, aiModelActive } = this.state;
 
     return (
-      <div className="App" style={{ height: screenHeight + 'px' }}> {/* 부모 요소인 div의 높이를 화면 높이로 조절 */}
+      <div className="App" style={{ height: screenHeight + 'px' }}>
         <h1>Video Player</h1>
         <video
           ref={this.videoRef}
+          onTimeUpdate={this.updateFrameNumber}
           onClick={this.handleVideoClick}
-          style={videoStyle} // 비디오 스타일 적용
+          style={videoStyle}
         />
         <div>
           <p>Frame: {frameNumber}</p>
         </div>
         <div>
           <input type="file" accept=".mp4,.mov" ref={this.fileInputRef} style={{ display: 'none' }} onChange={this.handleFileUpload} />
+          <input type="file" accept=".csv" ref={this.logFileInputRef} style={{ display: 'none' }} onChange={this.handleLogFileUpload} />
           <button onClick={() => this.fileInputRef.current.click()}>Upload Video</button>
-          <button onClick={this.togglePlayPause}>{videoPlaying ? 'Pause' : 'Play'}</button>
-          <button onClick={this.toggleAddingInfo}>{addingInfo ? 'Disable Adding Info' : 'Enable Adding Info'}</button>
+          <button onClick={() => this.logFileInputRef.current.click()}>Upload Log File</button>
+          {allFillUpload && (
+            <button onClick={this.runAIModel}>Run AI Model</button>
+          )}
+          <div>
+            {aiModelActive && (
+              <>
+                <button onClick={this.skipBackward}>{"<< 10 seconds backward"}</button>
+                <button onClick={this.togglePlayPause}>{videoPlaying ? 'Pause' : 'Play'}</button>
+                <button onClick={this.skipForward}>{">> 10 seconds forward"}</button>
+              </>
+            )}
+          </div>
+          <div>
+            {aiModelActive && (
+              <>
+                <button onClick={this.toggleAddingInfo}>{addingInfo ? 'Disable Adding Info' : 'Enable Adding Info'}</button>
+              </>
+            )}
+          </div>
+        </div>
+        <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '80%' }}>
+          {this.state.addingInfo && this.videoRef.current && (
+            <canvas
+              ref={this.canvasRef}
+              style={{ position: 'absolute', top: '0', left: '0', zIndex: 1, width: '100%', height: '100%' }}
+              onClick={this.handleVideoClick}
+            />
+          )}
         </div>
         <div>
           {pointDistances.map((distance, index) => (
-            <p key={index}>Distance {index + 1}: {distance.meters} meters ({distance.pixels} pixels)</p>
+            <p key={index}>Distance {index + 1}: {distance.meters} meters ({distance.pixels.toFixed(3)} pixels)</p>
           ))}
         </div>
+
       </div>
     );
   }
