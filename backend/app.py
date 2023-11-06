75479daac85b7d3d1854e994910d74e75987b0a8
강준형 <jh.kang@sk.com>
2023-11-06 21:34:51 +0900

    feat: backend 기본 app 추가

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from api import routers
+
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+    app.include_router(routers.video_router.router)
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", reload=True, port=8000, host="0.0.0.0")
