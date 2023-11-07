f00b12565ad8df3c0e5880777ddd2a5b3b122b56
강준형 <jh.kang@sk.com>
2023-11-07 20:33:13 +0900

    refactor: 아직 특별히 필요없는 디펜던시 관련 코드 삭제

--- models/sam_app_example/app.py
+++ models/sam_app_example/app.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
 
 from api import routers
-from api.services.container import get_container
 
 
 def create_app() -> FastAPI:
-    container = get_container(wire_modules=[routers.inference_router])
     app = FastAPI()
-    app.container = container
     app.include_router(routers.inference_router.router)
     return app
 
