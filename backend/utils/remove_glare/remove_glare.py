29c70ed10381d4a2479b7b27ef692308e453544b
roche-MH <roche5610@gmail.com>
2023-11-14 16:55:24 +0900

    feature: Added GPU application method to frame operation

--- backend/utils/remove_glare/remove_glare.py
+++ backend/utils/remove_glare/remove_glare.py
@@ -1,9 +1,9 @@
-from collections import deque
 import tqdm
 import cv2
 import numpy as np
 import argparse
 from typing import Union
+import torch
 
 class RGLARE:
     def __init__(self, video_path: str, queue_len: int, save:bool=True,
@@ -14,12 +14,16 @@ class RGLARE:
             self.video_save()
         self.total_frame = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
         self.queue_len = queue_len
-        self.frame_queue = deque(maxlen=self.queue_len)
+        self.frame_queue = []
         self.queue_full = False
         self.frame_count = 0
         self.frame_queue_done = 0
         self.gamma = gamma
-        self.weight = self.get_weight()
+        self.device= ('cuda:0' if torch.cuda.is_available() else 'cpu')
+        if self.device=='cuda:0':
+            self.weight=self.get_gpu_weight()
+        else:
+            self.weight = self.get_weight()
 
     def get_weight(self) -> np.ndarray:
         '''
@@ -31,6 +35,11 @@ class RGLARE:
             weight.append(weight[-1]-0.1)
         return np.array(weight)[:,None,None,None]
 
+    def get_gpu_weight(self) -> torch.Tensor:
+        weight=[1]
+        for _ in range(self.queue_len-1):
+            weight.append(weight[-1]-0.1)
+        return torch.Tensor(weight)[:,None,None,None].to(self.device)
 
     def video_save(self) -> None:
         '''
@@ -52,6 +61,96 @@ class RGLARE:
         '''
         gamma_corrected = np.power(frame[:,:,2],alpha)
         return gamma_corrected
+
+    def gamma_tensor_correction(self, frame, alpha:float=0.8):
+        gamma_corrected = torch.pow(frame[:,:,2],alpha)
+        return gamma_corrected
+
+    def video_gpu(self):
+        with tqdm.tqdm(total=self.total_frame, desc="GPU Remove Light") as pbar:
+            while self.frame_count < self.total_frame+self.queue_len:
+                ret, frame = self.cap.read()
+
+                if not ret:
+                    self.frame_queue_done+=1
+                    hsv_frame=None
+                    if self.frame_queue_done == self.queue_len:
+                        break
+                else:
+                    frame = cv2.medianBlur(frame,3)
+                    hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+                if self.queue_full:
+                    if hsv_frame is None:
+                        fused_frame, _ = torch.min(
+                            torch.stack(self.frame_queue, dim=0) *
+                            self.weight[:len(self.frame_queue),:,:,:],
+                            dim=0
+                        )
+                    else:
+                        tensor_frame = torch.tensor(hsv_frame/255.0).permute(2, 0, 1).to(self.device)
+                        self.frame_queue.append(tensor_frame)
+                        fused_frame, _ = torch.min(
+                            torch.stack(self.frame_queue, dim=0),
+                            dim=0
+                        )
+                    out_frame = self.frame_queue.pop(0)
+
+                    if self.gamma is True:
+                        out_frame[:,:,2] = self.gamma_tensor_correction(fused_frame)
+                    else:
+                        out_frame[:,:,2] = fused_frame[:,:,2]
+
+                    out_frame = torch.clamp(out_frame * 255.0, 0, 255).type(torch.uint8)
+                else:
+                    tensor_frame = torch.tensor(hsv_frame/255.0).permute(2,0,1).to(self.device)
+                    self.frame_queue.append(tensor_frame)
+                    if len(self.frame_queue) == self.queue_len - 1:
+                        self.queue_full = True
+                    continue
+
+                out_frame = out_frame.clone().detach().permute(1,2,0).cpu().numpy().astype(np.uint8)
+                out_frame = cv2.cvtColor(out_frame, cv2.COLOR_HSV2BGR)
+                self.out.write(out_frame)
+                pbar.update()
+
+    def frame_gpu(self) -> Union[np.ndarray]:
+        if self.queue_full:
+            ret, frame = self.cap.read()
+            if ret is None:
+                if len(self.frame_queue) == 0:
+                    return None
+                fused_frame, _ = torch.min(
+                    torch.stack(self.frame_queue) * self.weight[:len(self.frame_queue),:,:,:],
+                    axis=0)
+            else:
+                frame = cv2.medianBlur(frame,3)
+                hsv_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+                tensor_frame = torch.tensor(hsv_frame / 255.0).permute(2, 0, 1).to(self.device)
+                self.frame_queue.append(tensor_frame)
+                fused_frame, _ = torch.min(
+                    torch.stack(self.frame_queue) * self.weight,
+                    axis=0)
+            out_frame = self.frame_queue.pop(0)
+
+            if self.gamma is True:
+                out_frame[:,:,2] = self.gamma_tensor_correction(fused_frame)
+            else:
+                out_frame[:,:,2] = fused_frame[:,:,2]
+
+            out_frame = torch.clamp(out_frame * 255.0, 0, 255)
+
+            output_frame = out_frame.clone().detach().permute(1, 2, 0).cpu().numpy().astype(np.uint8)
+            output_frame = cv2.cvtColor(output_frame.astype('uint8'), cv2.COLOR_HSV2BGR)
+            return output_frame
+        else:
+            while len(self.frame_queue) != self.queue_len-1:
+                ret, frame = self.cap.read()
+                hsv_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+                tensor_frame = torch.tensor(hsv_frame / 255.0).permute(2, 0, 1).to(self.device)
+                self.frame_queue.append(tensor_frame)
+            self.queue_full=True
+            return self.frame_gpu()
 
     def t_run(self):
         '''
@@ -146,4 +245,3 @@ if __name__ == '__main__':
     parser.add_argument('--gamma', default=False, type=bool, required=True)
     args = parser.parse_args()
     s = RGLARE(args.video, args.ql, args.save, args.gamma)
-    k = s.t_run()
