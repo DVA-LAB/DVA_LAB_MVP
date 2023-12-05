9aea75da172df7880dced6e710d7b6a1b7a3e233
강준형 <jh.kang@sk.com>
2023-12-05 20:52:24 +0900

    refactor: 코드 포맷팅 수정

--- backend/api/routers/data_router.py
+++ backend/api/routers/data_router.py
@@ -1,19 +1,16 @@
 import glob
 import os
 import shutil
+import time
 
-from fastapi import (APIRouter, Depends, FastAPI, File, HTTPException,
+import torch
+from api.services.data_service import parse_videos_multithreaded
+from fastapi import (APIRouter, Depends, FastAPI, File, Form, HTTPException,
                      UploadFile, status)
 from fastapi.responses import FileResponse, JSONResponse
 from utils.log_sync.adjust_log import do_sync
 from utils.remove_glare import remove_glare
-from api.services.data_service import parse_videos_multithreaded
-from fastapi import APIRouter, UploadFile, File, Form, HTTPException
-import os
-import time
-import torch
 
-# import
 
 router = APIRouter(tags=["data"])
 
@@ -33,13 +30,11 @@ async def upload_video(file: UploadFile = File(...), preprocess: bool = Form(...
     delete_files_in_folder(video_path)
     delete_files_in_folder(frame_path)
     try:
-        file_location = os.path.join(
-            video_path, lowercase_extensions(file.filename)
-        )
+        file_location = os.path.join(video_path, lowercase_extensions(file.filename))
         with open(file_location, "wb") as file_object:
             shutil.copyfileobj(file.file, file_object)
         save_time = time.time()
-        print(f'video saving time: {round(save_time - s_time, 0)} sec')
+        print(f"video saving time: {round(save_time - s_time, 0)} sec")
 
         # TODO@jh: frame parsing을 background task로 처리하는 경우, 다 parsing되기전에 사용자 요청이 오는 경우 처리가 힘들지만 먼가 방법 고안이 필요함
         if preprocess:
@@ -48,20 +43,24 @@ async def upload_video(file: UploadFile = File(...), preprocess: bool = Form(...
             process_s_time = time.time()
             os.makedirs(processed_video_path, exist_ok=True)
             delete_files_in_folder(processed_video_path)
-            save_path = os.path.join(processed_video_path, lowercase_extensions(file.filename))
+            save_path = os.path.join(
+                processed_video_path, lowercase_extensions(file.filename)
+            )
             remover = remove_glare.RGLARE(file_location, save_path, 4, True, True)
             remover.video_gpu()
             process_f_time = time.time()
-            print(f'removing glare time: {round(process_f_time - process_s_time, 0)} sec')
+            print(
+                f"removing glare time: {round(process_f_time - process_s_time, 0)} sec"
+            )
             parse_videos_multithreaded(processed_video_path, frame_path)
-            print(f'frame parsing time: {round(time.time() - process_f_time)} sec')
+            print(f"frame parsing time: {round(time.time() - process_f_time)} sec")
 
         else:
             frame_s_time = time.time()
             parse_videos_multithreaded(video_path, frame_path)
-            print(f'frame parsing time: {round(time.time() - frame_s_time)} sec')
+            print(f"frame parsing time: {round(time.time() - frame_s_time)} sec")
 
-        print(f'total video upload time: {round(time.time() - s_time)} sec')
+        print(f"total video upload time: {round(time.time() - s_time)} sec")
         return {"message": "File saved successfully.", "filename": file.filename}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Could not save file: {e}")
@@ -88,16 +87,20 @@ async def get_video():
     file_location = os.path.join(video_storage_path, lowercase_extensions(videos[0]))
     return FileResponse(file_location)
 
+
 @router.get("/frame/{frame_number}")
 async def get_frame(frame_number: int):
     try:
-        frames = glob.glob(os.path.join(frame_path, '*.jpg'))
-        image_path = [x for x in frames if int(x.split('_')[-1].split('.')[0])==frame_number]
+        frames = glob.glob(os.path.join(frame_path, "*.jpg"))
+        image_path = [
+            x for x in frames if int(x.split("_")[-1].split(".")[0]) == frame_number
+        ]
         if not len(image_path):
             return JSONResponse(status_code=404, content={"message": "Frame not found"})
         return FileResponse(image_path[0])
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error accessing frame: {e}")
+
 
 @router.post("/csv/")
 async def upload_csv(file: UploadFile = File(...)):
