9ffb5941fd169b45fff7f77f46026018ceee8429
강준형 <jh.kang@sk.com>
2023-12-09 15:35:11 +0900

    refactor: 안쓰는 코드 삭제

--- models/sam_app_example/api/routers/__init__.py
+++
@@ -1,5 +0,0 @@
-from autologging import logged
-from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, BackgroundTasks, Depends, Response, status
-
-from . import inference_router  # noqa
