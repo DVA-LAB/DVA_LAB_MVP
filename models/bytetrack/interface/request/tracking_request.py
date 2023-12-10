c647977f31f4175f5f544696ccf5a01e93e0ab92
강준형 <jh.kang@sk.com>
2023-12-10 21:39:54 +0900

    feat: tracking request body 추가

---
+++ models/bytetrack/interface/request/tracking_request.py
@@ -0,0 +1,18 @@
+from autologging import logged
+from pydantic import BaseModel, Field
+
+__all__ = ["TrackingRequest"]
+
+
+@logged
+class TrackingRequest(BaseModel):
+    det_result_path: str = Field(..., description="detection result")
+    result_path: str = Field(..., description="tracking result")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "det_result_path": "/home/dva4/dva/backend/test/model/merged/result.txt",
+                "result_path": "/home/dva4/dva/backend/test/model/tracking/result.txt",
+            }
+        }
