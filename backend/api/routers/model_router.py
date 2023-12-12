601d5007a9f93f54cfb95fbda1549244a1a91312
강준형 <jh.kang@sk.com>
2023-12-12 19:52:32 +0900

    feat: api description 추가

--- backend/api/routers/model_router.py
+++ backend/api/routers/model_router.py
@@ -63,9 +63,16 @@ async def model_inference(body: ModelRequest):
     "/inference/detection",
     status_code=status.HTTP_200_OK,
     summary="sahi + yolo inference",
-    description="""이 엔드포인트는 Sahi와 YOLO 모델을 사용하여 객체 탐지를 수행합니다. 
-    사용자가 제공한 원본 프레임 경로(`img_path`), 결과를 저장할 경로(`csv_path`), 
-    그리고 inference과정에서 생성한 이미지 패치를 저장할 경로(`sliced_path`)를 입력 받습니다.""",
+    description="""\
+이 엔드포인트는 Sahi와 YOLO 모델을 사용하여 객체 탐지를 수행합니다. \
+사용자가 제공한 원본 프레임 경로(`img_path`), 결과를 저장할 경로(`csv_path`), \
+그리고 inference과정에서 생성한 이미지 패치를 저장할 경로(`sliced_path`)를 입력 받습니다.
+
+### 예시
+- `img_path`: "/home/dva4/dva/backend/test/frame_origin"
+- `csv_path`: "/home/dva4/dva/backend/test/model/detection/result.csv"
+- `sliced_path`: "/home/dva4/dva/backend/test/model/sliced"
+""",
 )
 async def inference_detection(img_path, csv_path, sliced_path):
     url = "http://localhost:8002/sahi/inference"
@@ -89,9 +96,16 @@ async def inference_detection(img_path, csv_path, sliced_path):
     "/inference/segmentation",
     status_code=status.HTTP_200_OK,
     summary="anomaly segmentation",
-    description="""이 엔드포인트는 anomaly 탐지 모델을 사용하여 segmentation mask를 생성합니다. 
-    사용자가 제공한 원본 프레임 경로(`img_path`), 프레임을 나누어 저장한 이미지 패치 경로(`sliced_path`),
-    그리고 결과를 저장할 (`output_path`)를 입력 받습니다..""",
+    description="""\
+이 엔드포인트는 anomaly 탐지 모델을 사용하여 segmentation mask를 생성합니다. \
+사용자가 제공한 원본 프레임 경로(`img_path`), 프레임을 나누어 저장한 이미지 패치 경로(`sliced_path`), \
+그리고 결과를 저장할 경로(`output_path`)를 입력 받습니다.
+
+### 예시
+- `frame_path`: "/home/dva4/dva/backend/test/frame_origin/DJI_0119_30_00000.jpg"
+- `slices_path`: "/home/dva4/dva/backend/test/model/sliced"
+- `output_path`: "/home/dva4/dva/backend/test/model/segment/DJI_0119_30_00000.jpg"
+""",
 )
 def inference_segmentation(frame_path, slices_path, output_path):
     url = "http://localhost:8003/anomaly/inference"
@@ -118,8 +132,14 @@ def inference_segmentation(frame_path, slices_path, output_path):
     "/inference/tracking",
     status_code=status.HTTP_200_OK,
     summary="object tracking",
-    description="""이 엔드포인트는 tracking모델을 사용하여 detection한 객체를 추적합니다. 
-    사용자가 제공한 detection 결과(`det_result_path`)를 입력받아서 결과를 (`result_path`)에 저장합니다.""",
+    description="""\
+이 엔드포인트는 tracking 모델을 사용하여 detection한 객체를 추적합니다. \
+사용자가 제공한 detection 결과(`det_result_path`)를 입력받아서 결과를 `result_path`에 저장합니다.
+
+### 예시
+- `det_result_path`: "/home/dva4/dva/backend/test/model/merged/result.txt"
+- `result_path`: "/home/dva4/dva/backend/test/model/tracking/result.txt"
+""",
 )
 def inference_tracking(detection_path, save_path):
     url = "http://localhost:8004/bytetrack/track"
