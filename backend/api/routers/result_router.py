cacd7b91aded63e3d2213c10b9b9d923a4b599fc
강준형 <jh.kang@sk.com>
2023-12-15 02:15:17 +0900

    feat: 모든 선박 포인트 (segement로 얻는 포인트 포함)에 대해서 gsd 계산하는 앤드포인트 추가

--- backend/api/routers/result_router.py
+++ backend/api/routers/result_router.py
@@ -1,13 +1,15 @@
 import glob
 import json
 import os
+import requests
+import json
 
 import requests
 from autologging import logged
 from fastapi import (APIRouter, Depends, FastAPI, File, Form, HTTPException,
                      UploadFile, status)
 from fastapi.responses import FileResponse, JSONResponse
-from interface.request import VisRequest
+from interface.request import VisRequest, VisRequestBev
 from utils.visualizing.visualize import (set_gsd, set_merged_dolphin_center,
                                          show_result)
 
@@ -36,6 +38,37 @@ async def model_inference(body: VisRequest):
     return body.output_video
 
 
+@router.post(
+    "/total_gsd",
+    status_code=status.HTTP_200_OK,
+    summary="visualizing result with BEV",
+)
+async def get_all_gsd(body: VisRequestBev):
+    gsds = dict()
+    with open(body.GSD_path, 'r') as f:
+        initial_frame, initial_gsd = f.read().split(' ')
+    gsds[initial_frame] = initial_gsd
+
+    with open(body.user_input, 'r') as f:
+        distance = float(f.read().split(' ')[-1])
+
+    ships_size = get_ship_size(body.user_input, body.frame_path, body.tracking_result)
+    for ship_size in ships_size:
+        frame = ship_size[0]
+        x1, y1, x2, y2 = ship_size[1:]
+        frame_file = [x for x in glob.glob(os.path.join(body.frame_path, '*.jpg')) if int(x.split('_')[-1].split('.')[0])==int(frame)][0]
+        gsd = get_gsd(frame, frame_file, x1, y1, x2, y2, distance)
+        gsds[frame] = gsd
+
+    sorted_gsds = dict(sorted(gsds.items()))
+
+    with open(body.GSD_path, 'w') as file:
+        for frame, gsd in sorted_gsds.items():
+            file.write(f'{frame} {gsd}\n')
+
+    return f'새롭게 계산한 GSD값이 {body.GSD_path}에 저장되었습니다.'
+
+
 @router.get(
     "/export/origin",
     status_code=status.HTTP_200_OK,
@@ -52,3 +85,48 @@ def delete_files_in_folder(folder_path):
     for file in files:
         if os.path.isfile(file):
             os.remove(file)
+
+def get_ship_size(user_input, frame_path, tracking_result):
+    url = 'http://112.216.237.124:8005/check_size'
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        "user_input": user_input,
+        "frame_path": frame_path,
+        "tracking_result": tracking_result
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    response_data = response.json()
+    return response_data
+
+def get_gsd(frame_number, frame_file, x1, y1, x2, y2, m_distance):
+    url = "http://112.216.237.124:8001/bev1"
+    headers = {"accept": "application/json", "Content-Type": "application/json"}
+    data = {
+        "frame_num": frame_number,
+        "frame_path": frame_file,
+        "csv_path": "/home/dva4/dva/backend/test/sync_csv/sync_log.csv",
+        "objects": [
+            None,
+            None,
+            None,
+            x1,
+            y1,
+            x2,
+            y2,
+            None,
+            -1,
+            -1,
+            -1,
+        ],
+        "realdistance": m_distance,
+        "dst_dir": "/home/dva4/dva/backend/test/frame_bev",
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    result = response.json()
+    if result[0] == 0:
+        return result[-1]
+    else:
+        return 0
